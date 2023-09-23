ea75d6c7868aca59fbf89c3a3a9a1cff3eee2a4b
Chun Ting Liew <cliew@c2r3s5.42singapore.sg>
2023-09-23 17:10:20 +0800

    Fail some of francisnetee test, included the log

---
+++ LIBFT/temp (copy)/libft/war-machine/dirlibft/tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+char	mapi_test(unsigned int i, char c){}
+
+
+// int main(){
+
+//     char str[]="hello";
+//     char *res;
+// 	// char *func=*ft_toupper;
+
+//     res=ft_strmapi(str,mapi_test);
+
+
+// }
